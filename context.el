11da5d0cd2157c589154c9fa44db214d55e37bd9
Bet4 <16643669+bet4it@users.noreply.github.com>
2018-06-27 17:49:14 +0800

    Integrate more with tabbar

--- context.el
+++ context.el
@@ -6,10 +6,8 @@
 (defvar context-orig-pos nil)
 (defvar context-base-pos nil)
 
-(defvar edit-window-buffer-list nil)
-
-(defvar navel--jumplist-list '())
-(defvar navel--jumplist-idx 0)
+(defvar navel--jumplist-list-prev '())
+(defvar navel--jumplist-list-next '())
 
 (defvar navel-get-context 'navel-elisp-get-func)
 
@@ -75,6 +73,13 @@
         ((bufferp position) position)
         (t nil)))
 
+(defun navel-buffer-in-edit (buf)
+  (let* ((tabset-alist-elf
+          (assq (window-number (navel-window-get 'edit))
+                tabbar-window-alist))
+         (buflist (cdr tabset-alist-elf)))
+    (assq buf buflist)))
+
 (defun navel-display-context (position)
   (let* ((context-same-buffer t)
          (new-buffer (navel-position-get-buffer position))
@@ -88,12 +93,14 @@
         (setq context-same-buffer nil)
         (when context-base-buffer
           (kill-buffer (window-buffer context-window))
-          (unless (member context-base-buffer edit-window-buffer-list)
+          (unless (or (navel-buffer-in-edit context-base-buffer)
+                      (get-buffer-window-list context-base-buffer nil t))
             (kill-buffer context-base-buffer)))
         (save-window-excursion
           (make-indirect-buffer new-buffer new-buffer-name t)))
       (with-selected-window context-window
         (switch-to-buffer new-buffer-name t t)
+        (setq header-line-format nil)
         (when new-point
           (use-local-map navel--context-map)
           (unless (and context-same-buffer
@@ -112,38 +119,55 @@
   (and (markerp context-orig-pos)
        (navel-jumplist-push-marker context-orig-pos))
   (navel-jumplist-push-marker (point-marker))
-  (navel-edit-minor-mode t)
-  (set (make-local-variable 'tool-bar-map) navel-tool-bar-map)
-  (add-to-list 'edit-window-buffer-list (current-buffer)))
+  (navel-edit-buffer-init)
+  (set (make-local-variable 'tool-bar-map) navel-tool-bar-map))
 
 (defun navel-jumplist-push-marker (marker)
-  (setq navel--jumplist-list
-        (nthcdr navel--jumplist-idx navel--jumplist-list))
-  (setq navel--jumplist-idx 0)
-  (push marker navel--jumplist-list))
+  (setq navel--jumplist-list-next '())
+  (push marker navel--jumplist-list-prev))
 
-(defun navel-jumplist-jump-idx (idx)
-  (let ((jumplist-curr (nth idx navel--jumplist-list)))
-    (and (marker-buffer jumplist-curr)
-         (switch-to-buffer (marker-buffer jumplist-curr))
-         (goto-char jumplist-curr))))
+(defun navel-jumplist-jump-marker (marker)
+  (and (marker-buffer marker)
+       (switch-to-buffer (marker-buffer marker))
+       (goto-char marker)
+       (recenter 6)))
 
 (defun navel-jumplist-backward ()
   (interactive)
-  (when (< navel--jumplist-idx (- (length navel--jumplist-list) 1))
-    (setq navel--jumplist-idx (+ navel--jumplist-idx 1))
-    (navel-jumplist-jump-idx navel--jumplist-idx)))
+  (setq navel--jumplist-list-prev
+        (cl-remove-if-not #'marker-buffer navel--jumplist-list-prev))
+  (cl-block nil
+    (while (> (length navel--jumplist-list-prev) 0)
+      (let ((prevm (pop navel--jumplist-list-prev)))
+        (push prevm navel--jumplist-list-next)
+        (unless (or (not (navel-buffer-in-edit (marker-buffer prevm)))
+                    (and (equal (marker-buffer prevm) (current-buffer))
+                         (< (count-lines (point-marker) prevm) 10)))
+          (navel-jumplist-jump-marker prevm)
+          (return t))))))
 
 (defun navel-jumplist-forward ()
   (interactive)
-  (when (> navel--jumplist-idx 0)
-    (setq navel--jumplist-idx (- navel--jumplist-idx 1))
-    (navel-jumplist-jump-idx navel--jumplist-idx)))
+  (setq navel--jumplist-list-next
+        (cl-remove-if-not #'marker-buffer navel--jumplist-list-next))
+  (cl-block nil
+    (while (> (length navel--jumplist-list-next) 0)
+      (let ((nextm (pop navel--jumplist-list-next)))
+        (push nextm navel--jumplist-list-prev)
+        (unless (or (not (navel-buffer-in-edit (marker-buffer nextm)))
+                    (and (equal (marker-buffer nextm) (current-buffer))
+                         (< (count-lines (point-marker) nextm) 10)))
+          (navel-jumplist-jump-marker nextm)
+          (return t))))))
 
 (defun navel-window-get (type &optional frame)
   (car (cl-remove-if-not #'(lambda (window)
                              (eql type (window-parameter window 'navel)))
                          (window-list frame))))
+
+(defun navel-disable-persp-frame (make-frame-func &rest args)
+  (let ((persp-init-new-frame-behaviour-override #'(lambda (&rest args) nil)))
+    (apply make-frame-func args)))
 
 (defun navel-tabbar-init ()
   (require 'aquamacs-tabbar)
@@ -152,8 +176,19 @@
               #'(lambda (&rest args)
                   (when (next-read-file-uses-dialog-p)
                     (apply 'read-file-name-default args))))
-  (tabbar-mode)
+  (advice-add #'tabbar-new-frame-with-clicked-buffer
+              :around #'navel-disable-persp-frame)
+  (advice-add #'tabbar-move-clicked-buffer-to-new-frame
+              :around #'navel-disable-persp-frame)
+  (fset 'ns-frame-is-on-active-space-p #'(lambda (frame) t))
+  (fset 'old-delete-window 'delete-window)
+  (fset 'frame-iconified-p 'frame-live-p)
+  (tabbar-init-tabsets-store)
   (remove-hook 'first-change-hook 'tabbar-window-update-tabsets-when-idle))
+
+(defun navel-edit-buffer-init ()
+  (navel-edit-minor-mode t)
+  (setq header-line-format tabbar-header-line-format))
 
 (defun navel-layout-init ()
   (delete-other-windows)
@@ -162,18 +197,18 @@
     (set-window-parameter win0 'navel 'edit)
     (set-window-parameter win1 'navel 'context)
     (with-selected-window win0
-      (navel-edit-minor-mode t))))
+      (navel-edit-buffer-init)
+      (navel-display-context (point-marker)))))
 
 (defun navel-context-init ()
   (interactive)
-  (navel-layout-init)
+  (navel-tabbar-init)
 
   (define-key navel--context-map (kbd "<mouse-1>") 'ignore)
   (define-key navel--context-map
     (kbd "<double-down-mouse-1>") 'navel-sync-context-to-edit)
 
-  (add-to-list 'edit-window-buffer-list (current-buffer))
-  (navel-tabbar-init))
+  (navel-layout-init))
 
 ;;;###autoload
 (define-minor-mode navel-edit-minor-mode nil
